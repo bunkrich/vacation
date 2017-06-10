514e9bf6818e54961aa4408d48f63c88b8dc1a67
Lana Chiad <email": null>
2017-06-10 18:21:17 -0400

    Add logged_in and current_user helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  helper_method :current_user
+  helper_method :logged_in?
 end
