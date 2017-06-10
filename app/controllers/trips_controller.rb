4801ad7ef1b3018da2add8f24244c4bc49bb35f4
Brent Unkrich <bunkrich@gmail.com>
2017-06-10 11:59:39 -0400

    updated the controller to pull the info and push the info to and from the database

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,46 @@
 class TripsController < ApplicationController
   def show
+    @trip = Trip.find_by(id: params[:id])
+    redirect_to "/trips" unless @trip
   end
 
   def new
   end
 
   def create
+    @trip = current_user.trips.new(trip_params)
+    if @trip.save
+      redirect_to @trip
+    else
+      @errors = @trip.errors.full_messages
+      render :new
+    end
   end
 
   def update
+    @trip = Trip.find_by(id: params[:id])
+      if @trip.update(trip_params)
+        redirect_to @trip
+    else
+      @errors = @trip.errors.full_messages
+      render :edit
+    end
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
+    redirect_to "/trips" unless @trip
   end
 
   def destroy
+    @trip = Trip.find_by(id: params[:id])
+    @trip.destroy
+
+    redirect_to trips_path
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:name, :location, :description, :start_date, :end_date)
   end
 end
