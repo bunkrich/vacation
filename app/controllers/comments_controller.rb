b083a5554c1510d98b98fa22eb925ff3df000243
Lana Chiad <email": null>
2017-06-10 16:50:06 -0400

    Add variables for comments#create, update, edit, and destroy; add comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,39 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to @trip
+    else
+      @errors = @comment.errors.full_messages
+      render :new
+    end
   end
 
   def update
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      redirect_to "/#{@comment.commentable_type.lowercase}s/#{@comment.commentable_id}"
+    else
+      @errors = @comment.errors.full_messages
+      render :edit
+    end
   end
 
   def edit
+    @comment = Comment.find_by(id: params[:id])
+    redirect_to "/#{@comment.commentable_type.lowercase}s/#{@comment.commentable_id}"
   end
 
   def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+
+    redirect_to '/'
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
