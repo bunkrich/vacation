4502cf39f93e9490a7dd8b2a071499a78352d02b
Lana Chiad <email": null>
2017-06-10 16:22:36 -0400

    Add comments to help differentiate which methods belongs on which view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  # User profile
   def show
     @user = User.find(params[:id])
     @trips = @user.trips
   end
 
+  # Trip profile
   def profile
     @user = User.find(params[:id])
     @flight = @user.items.find_by(category: "Flight")
