dd274d86dcd6c5be5c7fcba777c45a0c1c6b71b1
Lana Chiad <email": null>
2017-06-10 16:19:04 -0400

    Add lodge test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ item1 = user1.items.create(category: "Night Life", lookup: "A123bx34", body: "Ro
   comment3 = item1.comments.create(user: user2, body: "JK, I love it.")
 
 item2 = user1.items.create(category: "Flight", lookup: "AA123", body: "Flight to NYC", trip: trip1)
+lodge = user1.items.create(category: "Lodge", lookup: "Xe82e3", body: "Hotel in Chelsea", trip: trip1)
   item2.votes.create(user: user1, up_down: -1)
   item2.votes.create(user: user2, up_down: -1)
   comment4 = item2.comments.create(user: user1, body: "Cool Flight")
