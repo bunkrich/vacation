34fdbabc95bcf32c3cc0d7ee35cb5ef261a46ff9
Brent Unkrich <bunkrich@gmail.com>
2017-06-10 12:05:05 -0400

    updated the seeds to add comments to items AND the trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require 'faker'
 
 trip1 = Trip.create(name: "Party Weekend", location: "New York", description: "WOOOOO!")
+  commentA = trip1.comments.create(user: user1, body: "love New York")
+  commentB = trip1.comments.create(user: user2, body: "i HATE New York")
+  commentC = trip1.comments.create(user: user2, body: "JK, I love it.")
 
 user1 = User.create(email: "brent@gmail.com")
 user2 = User.create(email: "me@gmail.com" )
@@ -10,16 +13,16 @@ user2 = User.create(email: "me@gmail.com" )
 item1 = user1.items.create(category: "Night Life", lookup: "A123bx34", body: "Rooftop Bar", trip: trip1)
   item1.votes.create(user: user1, up_down: 1)
   item1.votes.create(user: user2, up_down: 1)
-  comment1 = trip1.comments.create(user: user1, body: "love to party")
-  comment2 = trip1.comments.create(user: user2, body: "i HATE to party")
-  comment3 = trip1.comments.create(user: user2, body: "JK, I love it.")
+  comment1 = item1.comments.create(user: user1, body: "love to party")
+  comment2 = item1.comments.create(user: user2, body: "i HATE to party")
+  comment3 = item1.comments.create(user: user2, body: "JK, I love it.")
 
 item2 = user1.items.create(category: "Flight", lookup: "AA123", body: "Flight to NYC", trip: trip1)
   item2.votes.create(user: user1, up_down: -1)
   item2.votes.create(user: user2, up_down: -1)
-  comment4 = trip1.comments.create(user: user1, body: "Cool Flight")
-  comment5 = trip1.comments.create(user: user2, body: "I'm on that flight too")
-  comment6 = trip1.comments.create(user: user2, body: "JK, I live here.")
+  comment4 = item2.comments.create(user: user1, body: "Cool Flight")
+  comment5 = item2.comments.create(user: user2, body: "I'm on that flight too")
+  comment6 = item2.comments.create(user: user2, body: "JK, I live here.")
   # comment7 = user1.comments.create(commentable_id: 1, commentable_type: "Trip" body: "I'm not going.")
 
 
