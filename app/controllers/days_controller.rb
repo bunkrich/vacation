33a8d564275390bccf49a6c6a1bd7f2fa8dffdbf
Natalie Ruiz <email": null>
2017-06-11 15:17:24 -0400

    Grab only the items for that day on day show page

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,5 +3,6 @@ class DaysController < ApplicationController
     @trip = Trip.find_by(id: params[:trip_id])
     @date = params[:id]
     @all_items = @trip.items
+    @day_items = @trip.items.where('date BETWEEN ? AND ?', @date.to_date.beginning_of_day, @date.to_date.end_of_day)
   end
 end
