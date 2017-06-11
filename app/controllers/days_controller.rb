25c8cd46cde553819d7e54d84351cdb388ce1340
Natalie Ruiz <email": null>
2017-06-11 15:18:27 -0400

    Erase static information

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,7 +2,6 @@ class DaysController < ApplicationController
   def show
     @trip = Trip.find_by(id: params[:trip_id])
     @date = params[:id]
-    @all_items = @trip.items
     @day_items = @trip.items.where('date BETWEEN ? AND ?', @date.to_date.beginning_of_day, @date.to_date.end_of_day)
   end
 end
