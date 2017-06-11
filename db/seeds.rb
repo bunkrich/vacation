1c1bb6c6485e40ff4e388c2a5cd4bade7fd5f27d
Natalie Ruiz <email": null>
2017-06-11 11:20:11 -0400

    add look up to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ require 'faker'
 
 trip1 = Trip.create(name: "Party Weekend", location: "New York", description: "WOOOOO!", start_date: 10.days.ago, end_date: 5.days.ago )
 
+trip2 = Trip.create(name: "Party Weekend", location: "New York", description: "WOOOOO!", start_date: 5.days.ago, end_date: 2.days.ago )
+
 
 user1 = User.create(email: "brent@gmail.com")
 user2 = User.create(email: "me@gmail.com" )
@@ -13,7 +15,7 @@ user2 = User.create(email: "me@gmail.com" )
   commentB = trip1.comments.create(user: user2, body: "i HATE New York")
   commentC = trip1.comments.create(user: user2, body: "JK, I love it.")
 
-item1 = user1.items.create(category: "Night Life", lookup: "A123bx34", body: "Rooftop Bar", trip: trip1, date: 10.days.ago.at_midday)
+item1 = user1.items.create(category: "Night Life", title: "Casino", lookup: "A123bx34", body: "Rooftop Bar", trip: trip1, date: 10.days.ago.at_midday)
   item1.votes.create(user: user1, up_down: 1)
   item1.votes.create(user: user2, up_down: 1)
   comment1 = item1.comments.create(user: user1, body: "love to party")
@@ -21,7 +23,7 @@ item1 = user1.items.create(category: "Night Life", lookup: "A123bx34", body: "Ro
   comment3 = item1.comments.create(user: user2, body: "JK, I love it.")
 
 item2 = user1.items.create(category: "Flight", lookup: "AA123", title: "My flight", body: "Flight to NYC", trip: trip1, date: 10.days.ago.at_midday)
-lodge = user1.items.create(category: "Lodge", lookup: "Xe82e3", body: "Hotel in Chelsea", trip: trip1)
+lodge = user1.items.create(category: "Lodge", lookup: "Xe82e3", title: "my hotel", body: "Hotel in Chelsea", trip: trip1, date: 10.days.ago.at_midday)
   item2.votes.create(user: user1, up_down: -1)
   item2.votes.create(user: user2, up_down: -1)
   comment4 = item2.comments.create(user: user1, body: "Cool Flight")
@@ -29,10 +31,11 @@ lodge = user1.items.create(category: "Lodge", lookup: "Xe82e3", body: "Hotel in 
   comment6 = item2.comments.create(user: user2, body: "JK, I live here.")
 
 
-item2 = user1.items.create(category: "restaurant", title:"yummy Restaurant 1" , body: "I really love the variety", trip: trip1, date: 9.days.ago.at_midday)
-item3 = user1.items.create(category: "Restaurant", title:"Another Restaurant", body: "Food is my favorite", trip: trip1, date: 9.days.ago.at_midday)
-item4 = user1.items.create(category: "Restaurant", title: "so much food", body: "YASSSS cookies", trip: trip1, date: 8.days.ago.at_midday)
-item5 = user1.items.create(category: "Restaurant", title: "Woohoo foood", body: "Cookies and milk", trip: trip1, date: 7.days.ago.at_midday)
+item3 = user1.items.create(category: "restaurant", title:"yummy Restaurant 1" , body: "I really love the variety", trip: trip1, date: 9.days.ago.at_midday)
+
+item4 = user1.items.create!(category: "Restaurant", title: "Another Restaurant", body: "Food is my favorite", lookup: "34234", trip: trip1, date: 8.days.ago.at_midday)
+item5 = user1.items.create!(category: "banana", title: "so much food", body: "YASSSS cookies", trip: trip1,lookup: "4324234", date: 7.days.ago.at_midday)
+item6 = user1.items.create!(category: "Restaurant", title: "Woohoo foood", body: "Cookies and milk", trip: trip1, lookup: "1234", date: 6.days.ago.at_midday)
 
 
 5.times do
