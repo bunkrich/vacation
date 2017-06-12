aa8e8a9a8f211ad317f9429618f644f2101142cc
Natalie Ruiz <email": null>
2017-06-12 10:50:59 -0400

    Add new and create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   # User profile
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+        if @user.save
+          log_in @user
+          redirect_to '/'
+        else
+          @errors = @user.errors.full_messages
+          render 'new'
+        end
+  end
+
   def show
     @user = User.find(params[:id])
     @trips = @user.trips
