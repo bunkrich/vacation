7c5d892c536ba1396b6b08a99c741413e57016f2
Natalie Ruiz <email": null>
2017-06-13 17:01:28 -0400

    Create helper method for user trips show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
   include SessionsHelper
+  include UsersHelper
 end
