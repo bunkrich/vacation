3d0989e2d477de4444cb9c2d9e227d78083a1966
Natalie Ruiz <email": null>
2017-06-13 17:50:32 -0400

    Fix helper for current_trip

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
   def current_trip
-    @current_trip ||= current_user.trip.where('start_date <= ?', Date.today).where('end_date >= ?', Date.today)
+    @current_trip ||= current_user.trips.where('start_date <= ?', Date.today).where('end_date >= ?', Date.today)
   end
 end
