9bc41f0a203b7c325b31be646ecb59e7e0c2bb87
Natalie Ruiz <email": null>
2017-06-13 10:05:32 -0400

    Permit password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name,:last_name,:email, :password, :image)
+      params.require(:user).permit(:first_name,:last_name,:email, :password, :image, :password_confirmation)
     end
 end
