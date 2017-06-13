d19347eab08ea6771e694e6bd3e0bbf7a7c3485c
Natalie Ruiz <email": null>
2017-06-13 10:06:36 -0400

    Add passowrd confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
 
   def full_name
     self.first_name + " " + self.last_name
