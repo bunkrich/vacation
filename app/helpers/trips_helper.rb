cddc9817d195524478c1e9294fb06273f652050e
Natalie Ruiz <email": null>
2017-06-14 11:33:06 -0400

    Only show times if for time has an item

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,22 @@
 module TripsHelper
+
+  def morning
+    morning = @day_items.where('date BETWEEN ? AND ?',@date.to_date.beginning_of_day, "#{@date} 13:59:59")
+  end
+
+  def day
+    day = @day_items.where('date BETWEEN ? AND ?',"#{@date} 14:00:00", "#{@date} 19:59:59")
+  end
+
+  def night
+    night = @day_items.where('date BETWEEN ? AND ?',"#{@date} 20:00:00", @date.to_date.end_of_day)
+  end
+
 end
+
+
+# morning is 8
+
+# afternoon is 2
+
+# night is 8
