828ae8cc19be848ce7ac1c9a8b9e6ee2d7c13d17
Natalie Ruiz <email": null>
2017-06-14 12:30:57 -0400

    Rename helper

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -12,8 +12,8 @@ module TripsHelper
     night = @day_items.where('date BETWEEN ? AND ?',"#{@date} 20:00:00", @date.to_date.end_of_day)
   end
 
-  def no_time
-    no_time = @day_items.where('DATE IS NULL')
+  def item_no_time
+    item_no_time = @day_items.where('DATE IS NULL')
   end
 
 
