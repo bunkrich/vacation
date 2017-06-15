f75fc6a1f5f48e8a224ad1b596e6a0f639d9f248
alexnhill <alexandrahillolds@gmail.com>
2017-06-15 12:27:52 -0400

    Placeholder for items without images now appearing

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   validates :category,:title, presence: true
+
+  before_save :placeholder_pic
+
+  def placeholder_pic
+  	if self.image == ""
+  		self.image = "http://svgur.com/i/1x4.svg"
+  	end
+  end
+
 end
